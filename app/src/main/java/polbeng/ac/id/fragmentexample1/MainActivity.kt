6d267256e3901b198f7ab3e34a6797f293acc972
blckguy <dni19877@gmail.com>
2022-05-19 12:21:55 +0700

    praktikum setelah uts

--- app/src/main/java/polbeng/ac/id/fragmentexample1/MainActivity.kt
+++ app/src/main/java/polbeng/ac/id/fragmentexample1/MainActivity.kt
@@ -4,17 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Coordinator {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun onBookChanged(index: Int) {
+    override fun onBookChanged(index:Int) {
         val frag = fragmentbookdescription
         if (frag is BookDescription) {
             frag.changeDescription(index)
         }
-
     }
+
 }
